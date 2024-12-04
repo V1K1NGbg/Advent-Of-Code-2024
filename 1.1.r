dd46f96285375c3cfe988004bc197542d89d28c1
V1K1NGbg <victor@ilchev.com>
2024-12-04 23:46:52 +0100

    Day 1: R

---
+++ 1.1.r
@@ -0,0 +1,15 @@
+# Rscript 1.1.r
+
+data = read.csv("input/1.csv")
+
+list1 <- data$list1
+list2 <- data$list2
+
+slist1 <- sort(list1)
+slist2 <- sort(list2)
+
+d <- abs(slist1 - slist2)
+
+sum_d <- sum(d)
+
+cat("", sum_d, "\n")
---
+++ 1.2.r
@@ -0,0 +1,16 @@
+# Rscript 1.2.r
+
+data = read.csv("input/1.csv")
+
+list1 <- data$list1
+list2 <- data$list2
+
+sum <- 0
+
+for (v in list1) {
+  n <- sum(list2 == v)
+  
+  sum <- sum + (v * n)
+}
+
+cat("", sum, "\n")

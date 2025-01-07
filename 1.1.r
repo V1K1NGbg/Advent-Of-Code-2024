b3631965185c4993bfa365b5880c053e385badab
V1K1NGbg <victor@ilchev.com>
2025-01-07 04:07:29 +0100

    Day 1: R

--- 1.1.r
+++ 1.1.r
@@ -1,6 +1,6 @@
 # Rscript 1.1.r
 
-data = read.csv("input/1.csv")
+data = read.csv("input/1.txt")
 
 list1 <- data$list1
 list2 <- data$list2
--- 1.2.r
+++ 1.2.r
@@ -1,6 +1,6 @@
 # Rscript 1.2.r
 
-data = read.csv("input/1.csv")
+data = read.csv("input/1.txt")
 
 list1 <- data$list1
 list2 <- data$list2
